ad8ecea129573a273303a44ba76a5e05102e56e5
hcimelle <44hcimelle@gmail.com>
2024-12-10 12:48:13 +0800

    20241210

--- demo-helloworld/src/Exercise20241210.java
+++ demo-helloworld/src/Exercise20241210.java
@@ -2,10 +2,19 @@ public class Exercise20241210 {
   public static void main (String[] args) {
     double priceForApple = 7.3;
     double priceForOrange = 6.5;
-    double quantityOfApple = 3;
-    double quantityOfOrange = 4;
+    int quantityOfApple = 3;
+    int quantityOfOrange = 4;
     //
     double totalAmount = priceForApple * quantityOfApple + priceForOrange * quantityOfOrange;
-    System.out.println(totalAmount);
+    System.out.println(totalAmount); //
+
+    int mathScore = 73;
+    int englishScore = 60;
+    int historyScore = 61;
+
+    double averageScore = (73.0 + 60.0 + 61.0) / 3;
+    System.out.println(averageScore); // 64.6666
+
   }
+
 }
