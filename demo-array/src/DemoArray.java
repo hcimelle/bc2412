015db0860c01e7e5b72634eb281880c09645b03a
hcimelle <44hcimelle@gmail.com>
2024-12-17 10:10:43 +0800

    update

--- demo-array/src/DemoArray.java
+++ demo-array/src/DemoArray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class DemoArray {
     public static void main(String[] args) {
         int x1 = 3;
@@ -93,7 +95,109 @@ public class DemoArray {
         }
         System.out.println(min); // -99
 
-    }
-}
 
+    // array - sum all elements
+    int[] arr6 = new int[] {9, 8, 99, 98};
+    sum = 0;
+    for (int i = 0; i < arr6.length; i++) {
+      sum = sum + arr6[i];
+    }
+    // swap
+    int left = 7;
+    int right = 9;
+    int temp = left; // backup left
+    left = right;
+    right = temp;
+    System.out.println(left); // 9
+    System.out.println(right); // 7
+
+    // array
+    int[] arr8 = new int[] {9, -8, 109, 99, 98};
+    for (int i = 0; i < arr8.length - 1; i++) {
+      System.out.println(arr8[i] + arr8[i + 1]);
+    }
+    // print 1 (9 + -8)
+    // print 101 (-8 + 109)
+    // ...
+    // print 197 (99 + 98)
+
+    // array swap
+    int[] arr7 = new int[] {9, -8, 109, 99, 98};
+    // move max value to the tail
+    // for loop + swap
+    // 9, -8, 109, 99, 98
+    // -8, 9, 109, 99, 98
+    // -8, 9, 109, 99, 98
+    // -8, 9, 99, 109, 98
+    // -8, 9, 99, 98, 109
+    for (int i = 0; i < arr7.length - 1; i++) {
+      if (arr7[i] > arr7[i + 1]) {
+        temp = arr7[i];
+        arr7[i] = arr7[i + 1];
+        arr7[i + 1] = temp;
+      }
+    }
+    System.out.println(Arrays.toString(arr7)); // [x, x, x, x, 109]
+
+    Integer[] arr11 = new Integer[] {9, 6, 4};
+    // Products for all numbers
+    int product = 1;
+    for (int i = 0; i < arr11.length; i++) {
+      product *= arr11[i];
+    }
+    System.out.println(product); // 216
+
+    double[] prices = new double[] {8.2, 6.5, 10.5};
+    int[] quantities = new int[] {9, 8, 3};
+    // 8.2 x 9 + 6.5 x 8 + 10.5 x 3
+    double totalAmount = 0.0;
+    for (int i = 0; i < prices.length; i++) {
+      totalAmount += prices[i] * quantities[i];
+    }
+    System.out.println(totalAmount); // 157.3
+
+    String s = String.valueOf(123);
+    System.out.println(s); // "123"
+    s = String.valueOf(true);
+    System.out.println(s); // "true"
+
+    System.out.println(String.valueOf('A')); // "A"
+
+    Integer i1 = Integer.valueOf("123");
+    System.out.println(i1); // 123
+
+    // Integer i2 = Integer.valueOf("h"); // java.lang.NumberFormatException
+
+    // "hello" -> 'h' 'e' 'l' 'l' 'o'
+    char[] chArr = "hello".toCharArray();
+    // olleh
+    char cTemp;
+    for (int i = 0; i < chArr.length / 2; i++) {
+      // chArr[i] vs chArr[chArr.length - i]
+      cTemp = chArr[i];
+      chArr[i] = chArr[chArr.length - 1 - i];
+      chArr[chArr.length - 1 - i] = cTemp;
+    }
+    String result = "";
+    for (int i = 0; i < chArr.length; i++) {
+      result += chArr[i];
+    }
+    System.out.println(result); // olleh
+
+    // h -> i, e -> f, l -> m, o -> p
+    // ifmmp
+    chArr = "hello".toCharArray();
+    for (int i = 0; i < chArr.length; i++) {
+      chArr[i] = (char) (chArr[i] + 1);
+    }
+    System.out.println(String.valueOf(chArr));  // "ifmmp", char Array -> String
+
+    // assume we have small letter ONLY
+    char[] arr12 = new char[] {'p', 'a', 'p', 'b', 'a', 'p'};
+    // more than one loop
+    System.out.println(maxNumChar); // p
+
+
+  }
+}
 
